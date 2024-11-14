2cbb45ff560841cf6d394f04841c3d7ae02c8d8c
Phathdt <phathdt379@gmail.com>
2024-11-14 09:39:11 +0700

    fix build

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,12 @@
-import getStroke from 'perfect-freehand';
+import getStroke from 'perfect-freehand'
 
-import { Point } from './types';
+import { Point } from './types'
 
 export const getSvgPathFromStroke = (points: Point[]): string => {
   const stroke = getStroke(points.map((p) => [p.x, p.y, p.pressure || 0.5]))
   if (!stroke.length) return ''
 
-  const d = stroke.reduce((acc, [x0, y0], i, arr) => {
-    const [x1, y1] = arr[(i + 1) % arr.length]
-    acc.push(i === 0 ? `M ${x0} ${y0}` : `L ${x0} ${y0}`)
-    return acc
-  }, [] as string[])
-
-  return d.join(' ')
+  return stroke.map(([x, y], i) => `${i === 0 ? 'M' : 'L'} ${x} ${y}`).join(' ')
 }
 
 export const MAX_HISTORY_LENGTH = 50
